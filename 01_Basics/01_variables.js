8ad805f7d336bf50eb4eac4f1e1412976928ec08
abhishek-kashyap-glitch <abhishek.kashyap@inves21.com>
2025-08-26 20:17:53 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "abc@inves21.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+
+// accountId = 2 // Not Allowed - Assignment to constant variable.
+accountEmail = "bcd@inves21.com"
+accountPassword = "54321"
+accountCity = "Hyderabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all JS code as newer version of JS
+
+// alert("Hello World@ There is an error!") // We are using the Node JS, not the browser
+
+let name = "Abhishek";
+let age = 25;
+let isLoggedIn = false;
+
+// Number => 2 to power 53
+// Bigint
+// String => "abc"
+// Boolean => True/False
+// Null => Standalone value
+// Undefined => Value not assigned
+// Symbol => Uniqueness
+// Object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
